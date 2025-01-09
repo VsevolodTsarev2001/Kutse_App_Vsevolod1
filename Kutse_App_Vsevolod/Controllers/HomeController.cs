e7e64322bb47858d4f42f7ad4d14afb23516c664
opilane <opilane@HP-CZC2349HTY>
2025-01-09 09:59:44 +0200

    new email

--- Kutse_App_Vsevolod/Controllers/HomeController.cs
+++ Kutse_App_Vsevolod/Controllers/HomeController.cs
@@ -44,7 +44,7 @@ namespace Kutse_App_Vsevolod.Controllers
                 WebMail.SmtpPort = 587;
                 WebMail.EnableSsl = true;
                 WebMail.UserName = "seva.tsarev@gmail.com";
-                WebMail.Password = "dhuj iozk xjwk sugy";
+                WebMail.Password = "*";
                 WebMail.From = "seva.tsarev@gmail.com";
                 WebMail.Send("seva.tsarev@gmail.com", "Vastus kutsele", guest.Name + " Vastus" + ((guest.WillAttend ?? false) ?
                     "tuleb poele " : "ei tule poele"));
